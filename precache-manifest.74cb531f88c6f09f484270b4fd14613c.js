793a383aebff4117cda17d6139ed2c1b83b23ad4
Nikhil Agarwal <nikhilagarwalassisi@gmail.com>
2019-11-08 19:37:11 +0530

    Updates

---
+++ precache-manifest.74cb531f88c6f09f484270b4fd14613c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "161064adfc499352f5489c0b39680287",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "e5629b38e81fc26e0403",
+    "url": "/calculator/static/css/main.b49c8199.chunk.css"
+  },
+  {
+    "revision": "c5dff8e663aeef0f1459",
+    "url": "/calculator/static/js/2.6532b9fa.chunk.js"
+  },
+  {
+    "revision": "e5629b38e81fc26e0403",
+    "url": "/calculator/static/js/main.5bc0f3c4.chunk.js"
+  },
+  {
+    "revision": "36931c26e901ffb0858c",
+    "url": "/calculator/static/js/runtime-main.a9f36f8d.js"
+  }
+]);
